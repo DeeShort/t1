576016b1a64b12919a63ba67f0ca1c11190ed7c7
DmitriiSc <deedisp@yahoo.com>
2019-09-20 11:54:31 +0200

    20.09 fast fertig fragen mit termin dpeicherung noch geblieben

---
+++ content/kalender.php
@@ -0,0 +1,104 @@
+
+<html>
+    <head>
+        <style>
+            .bar{
+                height:20px; width:98%;                 
+               }
+             .red{background-color: red;}
+             .green{background-color: greenyellow;}
+             .blau{background-color: blueviolet;}
+             .transit{
+                 width: 5%;  
+                 height: 20px;
+                 background: red;  
+                 -webkit-transition: width 2s; /* For Safari 3.1 to 6.0 */  
+                 transition: width 0.2s;
+             }
+             .transit:hover{
+                 width: 100%;
+             }
+        </style>
+    </head>
+    <body>
+        <table width="90%" border="1" class>
+            <tr>
+            <th colspan="8">KW 45 9. November - 13.November 2019</th>
+            </tr>
+            <tr>
+            <th>Trainer</th>
+            <th>Montag 9.11</th>
+            <th>Dienstag 10.11</th>
+            <th>Mittwoch 11.11</th>
+            <th>Donnerstag 12.11</th>
+            <th>Freitag 13.11</th>
+            <th>Samstag 14.11</th>
+            <th>Sonntag 15.11</th>
+            </tr>
+            <tr>
+            <th>Max Musterman</th>
+            <th class="red"> &nbsp;</th>
+            <th class="green"> &nbsp;</th>
+            <th class="blau"> &nbsp;</th>
+            <th class="blau"> &nbsp;</th>
+            <th class="blau"> &nbsp;</th>
+            <th class="blau"> &nbsp;</th>
+            <th class="blau"> &nbsp;</th>
+            >
+                       
+            </tr>  
+            <tr>
+            <th>Max Musterman</th>
+            <td colspan="7"><div class ="transit"></div></td>                                
+            </tr>
+            <tr>
+            <th>Max Musterman</th>
+            <td colspan="7"><div class ="transit"></div></td>                                
+            </tr>
+            <th>Max Musterman</th>
+            <td colspan="7"><div class ="transit"></div></td>                                
+            </tr>
+            <tr>
+            <th>Max Musterman</th>
+            <td colspan="7"><div class ="transit"></div></td>                                
+            </tr>
+            <th>Max Musterman</th>
+            <td colspan="7"><div class ="transit"></div></td>                                
+            </tr>
+            <tr>
+            <th>Max Musterman</th>
+            <td colspan="7"><div class ="transit"></div></td>                                
+            </tr>
+            <th>Max Musterman</th>
+            <td colspan="7"><div class ="transit"></div></td>                                
+            </tr>
+            <tr>
+            <th>Max Musterman</th>
+            <td colspan="7"><div class ="transit"></div></td>                                
+            </tr>
+            <th>Max Musterman</th>
+            <td colspan="7"><div class ="transit"></div></td>                                
+            </tr>
+            <tr>
+            <th>Max Musterman</th>
+            <td colspan="7"><div class ="transit"></div></td>                                
+            </tr>
+            <th>Max Musterman</th>
+            <td colspan="7"><div class ="transit"></div></td>                                
+            </tr>
+            <tr>
+            <th>Max Musterman</th>
+            <td colspan="7"><div class ="transit"></div></td>                                
+            </tr>
+           
+            <tr>
+            <th>Max Musterman</th>
+            <td colspan="7"><<progress class="bar" value="27" max="100"></progress>></td>                                
+            </tr>
+        
+    </table>
+    </body>
+</html>
+
+
+
